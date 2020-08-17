8debe1976192f35405b9cd7a1fff2d84a51cee70
vonbonin <vonbonin@infoware.de>
2020-08-17 11:44:19 +0200

    Inital Commit

---
+++ app/src/main/java/de/infoware/followmesdkexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.infoware.followmesdkexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
