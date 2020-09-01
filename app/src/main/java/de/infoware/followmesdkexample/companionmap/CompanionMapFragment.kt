15201e1e9d9468dff379c4eae2dfb1d7415dee86
vonbonin <vonbonin@infoware.de>
2020-09-01 15:16:14 +0200

    [Version 1 - Commenting and Cleanup]
    -commented CompanionMapViewModel.kt & CompanionMapFragment.kt
    -started JavaDocs

--- app/src/main/java/de/infoware/followmesdkexample/companionmap/CompanionMapFragment.kt
+++ app/src/main/java/de/infoware/followmesdkexample/companionmap/CompanionMapFragment.kt
@@ -21,17 +21,22 @@ import kotlinx.android.synthetic.main.companion_map_fragment.*
  *  includes the mapviewer and the map-element
  *  Listens to changes about the map (perspective changes, setting the center, etc.)
  */
+@Suppress("DEPRECATION")
 class CompanionMapFragment : Fragment() {
 
-    private val TAG = "CompanionMapFragment"
+    //private val TAG = "CompanionMapFragment"
 
     companion object {
         fun newInstance() = CompanionMapFragment()
     }
 
+    // The ViewModel used for this Fragment
     private lateinit var viewModel: CompanionMapViewModel
+    // The Filename which was selected in the FileListFragment
     private lateinit var selectedFileName: String
+    // Option if the FollowMeTour should start as a simulation
     private var isSimulating: Boolean? = null
+    // The Map-Element
     private lateinit var mapView: IwMapView
     private lateinit var mapViewer: Mapviewer
 
@@ -62,24 +67,42 @@ class CompanionMapFragment : Fragment() {
 
         initListener()
 
+        /**
+         *  Registers the ViewModel as the Listener for Navigation and Text-to-Speech Events
+         *  ViewModel needs to implement the NavigationListener & MapTripTTSListener Interface
+         *  TTS needs to be enabled explicitly, and can be disabled in the same way
+         */
         Navigation.registerNavigationListener(viewModel)
         MaptripTTSManager.Instance()?.setListener(viewModel)
         MaptripTTSManager.Instance()?.enableTTS(requireActivity().applicationContext)
     }
 
+    /**
+     *  Initialises the Listener for ViewModel Observables and MapViewer-Ready
+     */
     private fun initListener() {
+
+        /**
+         *  Observer for changes on the MapPerspective
+         *  When a new perspective is received, it gets set via the IwMapViewer
+         */
         val perspectiveListener = Observer<MapPerspective> { newPerspective ->
             mapViewer.perspective = newPerspective
         }
-
         viewModel.currentPerspective.observe(this.viewLifecycleOwner, perspectiveListener)
 
+        /**
+         *  Observer for Click-Event of the 'Center to Vehicle'-Button in the MapControlsFragment
+         *  Gets called anytime the Button is pressed and passed through the ViewModel
+         */
         val autoZoomObserver = Observer<Any> {
             mapViewer.resumeLocationTracking()
         }
-
         viewModel.autozoomToPosition.observe(this.viewLifecycleOwner, autoZoomObserver)
 
+        /**
+         *  Listener which gets called as soon as the MapViewer is ready to use
+         */
         mapView.setOnMapviewerReadyListener {
             mapViewer = mapView.mapviewer
             viewModel.initPerspective(mapViewer.perspective)
--- app/src/main/java/de/infoware/followmesdkexample/companionmap/CompanionMapViewModel.kt
+++ app/src/main/java/de/infoware/followmesdkexample/companionmap/CompanionMapViewModel.kt
@@ -12,68 +12,114 @@ import de.infoware.followmesdkexample.sound.MaptripTTSManager
 import java.math.BigDecimal
 import java.math.RoundingMode
 
+/**
+ *  ViewModel for CompanionMapFragment and MapControlsFragment
+ *  CompanionMapFragment and MapControlsFragment both use the same instance, so it can be used to pass information between them
+ *  Handles all logic related to the Map & MapViewer
+ *  Handles all Listener-Events for Navigation, TTS, FollowMe, and Tasks (such as calculating a route)
+ */
 class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListener,
     FollowMeRouteListener, TaskListener {
 
     private val TAG = "CompanionMapViewModel"
+
+    // The currently running FollowMeRoute
     private var currentFollowMeRoute: FollowMeRoute? = null
+    // The in the FileListFragment selected File (from the FollowMeFileRepo)
     private var selectedFile: FollowMeTour? = null
+    // Simulation-Option for the FollowMeRoute
     private var isSimulation = false
 
-    // welches Fragment welcher Observer
+    // LiveData for the Crossing-Information available via the crossingInfoReceived NavigationLister event
     val currentStreetName = MutableLiveData<String>()
     val nextStreetName = MutableLiveData<String>()
     val metersToCrossing = MutableLiveData<Double>()
     val secondsToCrossing = MutableLiveData<Int>()
-    val progress = MutableLiveData<Double>()
     val pictoFileName = MutableLiveData<String>()
 
+    // LiveData for the progress of calculating the route
+    val progress = MutableLiveData<Double>()
+
+    // LiveData for the Destination-Information available via the destinationInfoReceived NavigationLister event
     val metersToDestination = MutableLiveData<Int>()
     val destinationReached = MutableLiveData<Int>()
 
+    // LiveData for the current Sound-Option, so that the MapControlsFragment can set the visual accordingly
     val currentMuteOption = MutableLiveData<Boolean>()
+
+    // LiveData for the MapPerspective. Gets called via a button in the MapControlsFragment and sends the next perspective to the CompanionMapFragment
     val currentPerspective = MutableLiveData<MapPerspective>()
+    // LiveData for the 'Zoom to Vehicle'-Button. Gets called when the button (MapControlsFragment) is clicked and triggers the Observer in the CompanionMapFragment
     val autozoomToPosition = MutableLiveData<Any>()
 
+    /**
+     *  Initialises the MapPerspective. Needed for calculating the next perspective on button-click
+     *  @param perspective The current MapPerspective
+     */
     fun initPerspective(perspective: MapPerspective) {
         this.currentPerspective.postValue(perspective)
     }
 
+    /**
+     *  Starts the calculation of a FollowMeTour and saves the Tour
+     *  Registers the TaskListener for the Calculation-Progress
+     *  @param filename The FileName in the /user/routes/ directory. Gets used to fetch the selected File from the FileListRepo
+     *  @param simulating Option if the Tour should be simulation or a normal navigation
+     */
     fun startFollowMeTour(filename:String, simulating: Boolean = false) {
         if(filename != "") {
-            // SDK Funktion
+
+            /**
+             *  Allows to create a runnable that will execute on the MapTrip SDK thread and bundle multiple consecutive calls to be executed in a single Task
+             */
             ApiHelper.Instance().queueApiCall(Runnable {
                 FollowMeRoute.registerFollowMeRouteListener(this)
 
+                // fetches the selected File from the Repo
                 selectedFile = FollowMeFileRepo.getFileByName(filename)
 
                 if(selectedFile != null) {
+                    // Initialises the FollowMeRoute via the filePath. The initialised Route gets returned in the .init() - Function and saved as a global variable
                     currentFollowMeRoute = FollowMeRoute.init(selectedFile!!.filePath)
 
+                    // Logs an Error if the FollowMeRoute could not be initialised
                     if(currentFollowMeRoute == null) {
                         Log.e(TAG, Api.getLastError().toString())
                         return@Runnable
                     }
 
-                    // SDK Funktion
+                    // Registers the ViewModel as TaskListener for the FollowMeRoute tasks (e.g. calculating the tour)
                     currentFollowMeRoute!!.registerTaskListener(this)
                     currentFollowMeRoute!!.calculate(true, false)
+
+                    // sets the global variable
                     isSimulation = simulating
                 }
             })
         }
     }
 
+    /**
+     *  Triggers the Observable to send an Any-Object to the CompanionMapFragment
+     *  CompanionMapFragment set the MapViewer -Center on the Vehicle
+     *  Gets called from the MapControlsFragment on Button-Click
+     */
     fun autoZoomToCurrentPosition() {
         this.autozoomToPosition.postValue(Any())
     }
 
+    /**
+     *  Calculates the new map-perspective and sends the result via an Observable to the CompanionMapFragment
+     *  Gets called from the MapControlsFragment on Button-Click
+     *  Needs the initPerspective() - Function to be called before, to set the initial value
+     */
     fun switchPerspective() {
         val perspective = this.currentPerspective.value
         val newPerspective: MapPerspective?
 
         Log.d(TAG, "Current perspective: $perspective")
 
+        // Rotates through the available map-perspectives
         newPerspective = when (perspective) {
             MapPerspective.PERSPECTIVE_2D_DRIVING_DIRECTION ->
                 MapPerspective.PERSPECTIVE_2D_NORTHWARD
@@ -90,15 +136,22 @@ class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListene
         this.currentPerspective.postValue(newPerspective)
     }
 
+    /**
+     *  Switches the MuteOption of MapTripTTS and send the result to the MapControlsFragment
+     *  Gets called from the MapControlsFragment on Button-Click
+     */
     fun switchMuteOption() {
         val isMute = MaptripTTSManager.Instance()?.isMute()
         currentMuteOption.postValue(isMute!!.not())
         MaptripTTSManager.Instance()?.setMute(isMute.not())
     }
 
+    // TaskListener Callbacks
 
-    // LISTENER FUNKTIONEN
-    // TODO VAR UMBENNEN
+    /**
+     *  Gets called when a task is finished (e.g. the Tour is finished calculating)
+     *  @param task The finished Task
+     */
     override fun taskFinished(task: BaseTask) {
         if(task.returnValue == ApiError.OK) {
             currentFollowMeRoute!!.start(isSimulation)
@@ -106,6 +159,10 @@ class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListene
         }
     }
 
+    /**
+     *  Gets updated when a new task progress is made
+     *  @param task The currently running Task
+     */
     override fun taskProgress(task: BaseTask) {
         if(task.returnValue == ApiError.OK) {
             val roundedProgress = BigDecimal(task.progress).setScale(2, RoundingMode.HALF_EVEN).toDouble()
@@ -113,6 +170,13 @@ class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListene
         }
     }
 
+    // FollowMeRouteListener callbacks
+
+    /**
+     *  Gets called on FollowMeAction (e.g. 'collect left', 'transfer', etc)
+     *  @param actionType the Type of the FollowMeAction
+     *  @param eventString the text which gets used by the TTS
+     */
     override fun followMeAction(actionType: FmrActionType?, eventString: String?): Boolean {
         if(eventString != null) {
             Log.d(TAG, "followMeAction")
@@ -123,6 +187,11 @@ class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListene
         return true
     }
 
+    /**
+     *  Gets called on FollowMeEvent (e.g. 'backwards', 'forwards', etc; user generated events)
+     *  @param eventType the ID if the Event
+     *  @param eventString the text which gets used by the TTS
+     */
     override fun followMeEvent(eventType: Int, eventString: String?): Boolean {
         if(eventString != null) {
             Log.d(TAG, "followMeEvent")
@@ -133,27 +202,60 @@ class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListene
         return true
     }
 
+    // NavigationListener callbacks
+
+    /**
+     *  Gets called on receiving a vehicle warning
+     *  @param restrictionType Specifies the type of restriction
+     *  @param restrictionValue the values of the specified restriction
+     */
     override fun vehicleWarningReceived(restrictionType: VehicleWarningType?, restrictionValue: Double) {
 
     }
 
+    /**
+     *  Gets called when a new / better Route is possible
+     *  @param routeComparison Object to get the differences between the current route and the new route. Can also be used to start the proposed new route
+     */
     override fun routeUpdate(routeComparison: RouteComparison?) {
 
     }
 
+    /**
+     *  Gets called when an automatic rerouting occurs; Needs a currently active Navigation
+     *  @param task Contains the rerouting process
+     */
     override fun rerouting(task: Task<Void>?) {
 
     }
 
+    /**
+     *  Gets called when a new lane information is available
+     *  @param allArrows all available arrows at the next crossing
+     *  @param divider lane divider
+     *  @param relevantArrows the relevant arrows at he next crossing
+     */
     override fun laneInfoReceived(allArrows: String?, divider: String?, relevantArrows: String?) {
 
     }
 
+    /**
+     *  Gets called when new information about the destination is available
+     *  @param secondsToDestination The E.T.A in seconds
+     *  @param metersToDestination the estimated meters to the destination
+     *  @param energyToDestination the estimated energy (kwh) needed to reach the destination
+     */
     override fun destinationInfoReceived(secondsToDestination: Double, metersToDestination: Double, energyToDestination: Double) {
         val metersAsInt = BigDecimal(metersToDestination).setScale(0, RoundingMode.HALF_EVEN).toInt()
         this.metersToDestination.postValue(metersAsInt)
     }
 
+    /**
+     *  Gets Called before a advice / direction indication is needed
+     *  @param speechIndependentSentence language independent speech sentence
+     *  @param additionalAdviceInfo streetname / routenumber responses
+     *  @param sentence speechIndependentSentence in target language including the additionalAdviceInfo
+     */
     override fun beforeAdviceStarts(speechIndependentSentence: String?, additionalAdviceInfo: String?, sentence: String?): Boolean {
 
         if(sentence != null) {
@@ -163,10 +265,22 @@ class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListene
         return true
     }
 
+    /**
+     *  Gets called when the navigation started successfully
+     */
     override fun navigationStarted() {
 
     }
 
+    /**
+     *  Gets called when new info about the next crossing is available
+     *  @param actualStreetName The street name the device is currently on
+     *  @param nextStreetName The next street name after the crossing
+     *  @param pictoFileName The name of the pictogram picture file
+     *  @param streetType Type of street / crossing
+     *  @param metersToCrossing distance to the crossing in meters
+     *  @param secondsToCrossing E.T.A to crossing in seconds
+     */
     override fun crossingInfoReceived(
         actualStreetName: String?,
         nextStreetName: String?,
@@ -175,9 +289,12 @@ class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListene
         metersToCrossing: Double,
         secondsToCrossing: Double
     ) {
+        // Rounds the meters to 2 decimal places
         val roundedMeters = BigDecimal(metersToCrossing).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+        // Cuts of the milliseconds of the secondsToCrossing
         val roundedSeconds = secondsToCrossing.toInt()
 
+        // Triggers the Observers and posts new data for the MapControlFragment to handle
         this.pictoFileName.postValue(pictoFileName)
         this.nextStreetName.postValue(nextStreetName)
         this.currentStreetName.postValue(actualStreetName)
@@ -185,19 +302,38 @@ class CompanionMapViewModel : ViewModel(), NavigationListener, MaptripTTSListene
         this.secondsToCrossing.postValue(roundedSeconds)
     }
 
+    /**
+     *  Gets called when a destination is reached
+     *  @param index The index of the destination (0 based; e.g. returns 2 if it was the third destination reached)
+     */
     override fun destinationReached(index: Int) {
+        // Posts new data so the MapControlFragment knows the Navigation has been finished
         this.destinationReached.postValue(index)
         Log.d(TAG, "DestinationReached: $index")
     }
 
+    /**
+     *  Gets called when a new speedLimit is available
+     *  @param limit the speed limit in km/h (a value <= 0 signals that no speed limit is available at the moment)
+     */
     override fun speedLimitReceived(limit: Double) {
 
     }
 
+    // MapTripTTSListener callbacks
+
+    /**
+     *  Gets called when the initialization was successful
+     */
     override fun ttsInitSuccessful() {
         Log.d(TAG, "ttsInit Successful")
     }
 
+    /**
+     *  Gets called when there was an error in the initialization
+     *  @param missingData boolean if there was missing data
+     *  @param notSupported boolean if the TTS is not supported
+     */
     override fun ttsInitError(missingData: Boolean, notSupported: Boolean) {
         Log.e(TAG, "ttsInit error: MissingData: $missingData - notSupportet: $notSupported")
     }
